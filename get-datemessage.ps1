705bda577d91e398cf40fb30df02b49e56b87119
Kevin Laszuk <160932781+KevinLaszuk@users.noreply.github.com>
2025-04-27 16:40:20 -0400

    Add files via upload

---
+++ get-datemessage.ps1
@@ -0,0 +1,15 @@
+﻿
+A basic PowerShell script to print a welcome message along with the date and
+time at the top of the PowerShell CLI prompt
+
+Created 4-15-2024 - KL
+
+cmdlet name: get-datemessage.ps1
+
+write-host " "
+write-host "Welcome to KL-SP24-S22-1" -ForegroundColor Red -BackgroundColor White
+write-host " "
+write-host "Today is: "-ForegroundColor Blue -NoNewline
+$date = Get-Date
+write-host $date -ForegroundColor Yellow
+write-host " "
